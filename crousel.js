ccf09ea17f28e445a1097f15dc7b6514cbf7cfa1
rohitsingh-23 <rohit23498@gmail.com>
2021-12-15 23:06:48 +0530

    40% complete

---
+++ crousel.js
@@ -0,0 +1,19 @@
+// const track = document.getElementsByClassName('crousel_track')
+// // let track = document.querySelector(".crousel_track")
+// // let slides = track;
+// // const slides = Array.from(track.children)
+// console.log(track)
+// console.log(slides)
+
+product = [{
+    img1: "",
+    img2: "",
+    title: "",
+    des: "",
+    price: 6390,
+    varient:true,
+    vimg1 : "",
+    vimg2: "",
+    vimg3: ""
+},{},{}]
+
