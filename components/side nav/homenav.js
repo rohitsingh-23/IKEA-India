92eac29abd071ca1f3096ce7cc6d98187de62847
Sanjeet Kumar Sangam <factsbe0@gmail.com>
2021-12-18 15:30:06 +0530

    push update

---
+++ components/side nav/homenav.js
@@ -0,0 +1,44 @@
+function nav(){
+  return `<div class="main">
+  <div class="cross-logo">
+    <div class="close"><ion-icon name="close-outline"></ion-icon></div>
+
+    <div class="logo1">
+      <img
+        src="https://www.ikea.com/in/en/static/ikea-logo.f7d9229f806b59ec64cb.svg"
+        alt=""
+      />
+    </div>
+  </div>
+  <div class="content-toggle">
+    <div class="h1">
+      <h1>Products</h1>
+      <h1>Rooms</h1>
+      <h1>New at IKEA</h1>
+      <h1>All offers</h1>
+    </div>
+
+    <div class="h1-bottom">
+      <p>Ideas</p>
+      <p>Currently at IKEA</p>
+      <p>Track Your Order</p>
+    </div>
+
+    <div class="bottom-most">
+      <p>IKEA Family</p>
+      <p>IKEA Business</p>
+      <p>Store</p>
+      <p>IKEA Restaurant</p>
+      <p>Shop by Series</p>
+      <p>Customer service</p>
+      <p>Design your room</p>
+      <p>My Profile</p>
+    </div>
+    <div>
+        <div class="globe"><ion-icon class="gicon" name="globe-outline"></ion-icon> <p>Change country</p> </d>
+        </div>
+  </div>
+</div>`
+}
+
+export default nav;
